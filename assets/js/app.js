022899bdbd9c02f6d008c07a4a1d3b3a4cca3517
Max Ramirez Martel <mramirez@imaginatics.info>
2017-08-01 16:55:47 -0500

    contact section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
 $(function(){
   menu();
+  hoverInput();
+  smoothScroll(800);
 });
+
+
+function smoothScroll (duration) {
+ $('a[href^="#"]').on('click', function(event) {
+
+     var target = $( $(this).attr('href') );
+
+     if( target.length ) {
+         event.preventDefault();
+         $('html, body').animate({
+             scrollTop: target.offset().top
+         }, duration);
+     }
+ });
+}
+
 
 function Countdown(start)
 {
@@ -17,9 +35,16 @@ function Countdown(start)
   });
 }
 
+function hoverInput(){
+  $(".form-wrapper form input, .form-wrapper form textarea").click(function(){
+    $(this).parent().addClass("active");
+      $(this).parent().siblings().removeClass("active");
+  });
+}
+
 function menu()
 {
-  var t = 0, s=0, e = 0, m=0, cl=0;
+  var t = 0, s=0, e = 0, m=0, cl=0, ct=0;
 
 
   $("#show-menu").click(function(){
@@ -63,8 +88,6 @@ function menu()
     if(wTop + $(window).height() > aboutTop + 300 && wTop + 200 < aboutTop + $(".about-section").height()){
       $(".about-content").addClass("show");
 
-    }else{
-      $(".about-content").removeClass("show");
     }
 
 
@@ -79,29 +102,20 @@ function menu()
         });
       }
       s++;
-    }else{
-      $(".image-service").removeClass("show");
-      s=0;
     }
 
     var numbersTop = $(".numbers-section").offset().top;
     if(wTop  > numbersTop - 150 && wTop + 500 < numbersTop + $(".numbers-section").height()){
       if(t==0){
         Countdown(0);
-        $("body, .numbers-wrapper, .bar-top").addClass("active-number");
       }
       t++;
       $(".numbers-content").addClass("show");
-    }else{
-      t=0;
-      $("body, .numbers-wrapper, .bar-top").removeClass("active-number");
-      $(".numbers-content").removeClass("show");
     }
 
     var teamTop = $(".team-section").offset().top;
     var teamCTop = $(".team-content").offset().top;
     if(wTop > teamTop - 150 && wTop + 300 < teamTop + $(".team-section").height()){
-      console.log(wTop + 600+"    == " + teamTop);
       $(".team-wrapper h1, .team-wrapper h3").addClass("show")
       if(wTop + $(window).height() > teamCTop + 300){
         if(e==0){
@@ -114,30 +128,15 @@ function menu()
         e++;
       }
     }
-    else{
-      e=0;
-      $(".team").removeClass("show");
-      $(".team-wrapper h1, .team-wrapper h3").removeClass("show")
-    }
 
     var mapTop = $(".map-section").offset().top;
 
-    if(wTop + 100 > mapTop && wTop + 420 < mapTop + $(".map-section").height()){
+    if(wTop + 200 > mapTop && wTop + 200 < mapTop + $(".map-section").height()){
       if(m==0){
-        $("body, .bar-top").addClass("active-map");
-        setTimeout(function(){
-          $("#map").addClass("show");
-        },600);
+        $("#map").addClass("show");
       }
       m++;
-    }else{
-      m=0;
-      setTimeout(function(){
-        $("#map").removeClass("show");
-      },100);
-        $("body, .bar-top").removeClass("active-map");
     }
-
 
 
 
@@ -150,18 +149,24 @@ function menu()
          $(".client").each(function( index ) {
            setTimeout(function(){
              $(".client").eq(index).addClass("show");
-           }, index*420);
+           }, index*300);
          });
       }
       cl++;
-    }else{
-      cl=0;
-      $(".client").removeClass("show");
-      $(".clients-wrapper h1, .clients-wrapper h3").removeClass("show");
-      $(".client-content i").removeClass("show");
-
     }
 
+
+
+
+
+    var conctactTop = $(".contact-section").offset().top;
+
+    if(wTop + 100 > conctactTop && wTop + 100 < conctactTop + $(".contact-section").height()){
+      if(ct==0){
+        $(".contact-wrapper").addClass("show");
+      }
+      ct++;
+    }
 
 
   });
